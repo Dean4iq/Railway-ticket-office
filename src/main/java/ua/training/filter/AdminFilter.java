a19e777d596daa4a54ca1b702dfc4403d56c083f
Dean4iq <denis.ua22@gmail.com>
2018-12-05 17:35:06 +0200

    Filters & log4j initial version added

--- src/main/java/ua/training/filter/AdminFilter.java
+++ src/main/java/ua/training/filter/AdminFilter.java
@@ -1,4 +1,33 @@
 package ua.training.filter;
 
-public class AdminFilter {
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AdminFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        final HttpServletRequest httpRequest = (HttpServletRequest) request;
+        final HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(true);
+
+        if (session.getAttribute("Admin") == null) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/exception");
+        } else {
+            filterChain.doFilter(httpRequest, httpResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
