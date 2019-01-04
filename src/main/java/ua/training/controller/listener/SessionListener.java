9930f3da46da8039f05e90db1ab10e192cf69d2e
Dean4iq <denis.ua22@gmail.com>
2019-01-04 23:50:59 +0200

    Transaction rollback in sessionDestroyed() added

--- src/main/java/ua/training/controller/listener/SessionListener.java
+++ src/main/java/ua/training/controller/listener/SessionListener.java
@@ -1,8 +1,14 @@
 package ua.training.controller.listener;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ua.training.model.service.PurchaseService;
+
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -10,6 +16,8 @@ import java.util.Set;
 
 
 public class SessionListener implements HttpSessionListener {
+    public static final Logger LOG = LogManager.getLogger(SessionListener.class);
+
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         Map<String, String> menuItems = new LinkedHashMap<>();
@@ -21,6 +29,9 @@ public class SessionListener implements HttpSessionListener {
                 .getSession().getServletContext().getAttribute("activeSessions");
         sessionSet.add(httpSessionEvent.getSession());
         httpSessionEvent.getSession().setAttribute("userbar", menuItems);
+
+        LOG.info("New session created");
+        LOG.debug("sessionCreated()");
     }
 
     @Override
@@ -29,20 +40,39 @@ public class SessionListener implements HttpSessionListener {
                 .getSession().getServletContext().getAttribute("loggedUsers");
         Set<HttpSession> sessionSet = (Set<HttpSession>) httpSessionEvent
                 .getSession().getServletContext().getAttribute("activeSessions");
-        sessionSet.add(httpSessionEvent.getSession());
 
+        checkTransactions(httpSessionEvent);
         sessionSet.remove(httpSessionEvent.getSession());
 
         if (httpSessionEvent.getSession().getAttribute("User") != null) {
             String userName = (String) httpSessionEvent.getSession()
                     .getAttribute("User");
             loggedUsers.remove(userName);
+
+            LOG.info("User {} has logged out", userName);
         } else if (httpSessionEvent.getSession().getAttribute("Admin") != null) {
             String userName = (String) httpSessionEvent.getSession()
                     .getAttribute("Admin");
             loggedUsers.remove(userName);
+
+            LOG.info("Admin {} has logged out", userName);
         }
 
-        httpSessionEvent.getSession().setAttribute("loggedUsers", loggedUsers);
+        LOG.debug("sessionDestroyed()");
+    }
+
+    private void checkTransactions(HttpSessionEvent httpSessionEvent){
+        if (httpSessionEvent.getSession().getAttribute("Ticket") != null
+                && httpSessionEvent.getSession().getAttribute("ticketConnection") != null) {
+
+            try (Connection connection =
+                         (Connection) httpSessionEvent.getSession().getAttribute("ticketConnection")) {
+                if (!connection.isClosed()) {
+                    PurchaseService.declinePurchasing(connection);
+                }
+            } catch (SQLException exception) {
+                LOG.error("Exception in sessionDestroyed(): {}", exception);
+            }
+        }
     }
 }
