109454c59cb5ef4c361c72234907816dac30eee0
Dean4iq <denis.ua22@gmail.com>
2019-01-06 02:34:12 +0200

    Both classes refactored

--- src/main/java/ua/training/controller/command/SearchCommand.java
+++ src/main/java/ua/training/controller/command/SearchCommand.java
@@ -2,23 +2,25 @@ package ua.training.controller.command;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ua.training.model.entity.Route;
+import ua.training.model.entity.Train;
 import ua.training.model.service.MethodProvider;
 import ua.training.model.service.SearchTrainService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Timestamp;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SearchCommand implements Command {
     private static final Logger LOG = LogManager.getLogger(SearchCommand.class);
     private static final Map<String, MethodProvider> COMMANDS = new HashMap<>();
 
     public SearchCommand() {
-        COMMANDS.putIfAbsent("trainNumberSubmit", SearchTrainService::findTrainById);
-        COMMANDS.putIfAbsent("trainDestinationSubmit", SearchTrainService::findTrainByRoute);
-        COMMANDS.putIfAbsent("allTrainSubmit", SearchTrainService::findAllTrains);
+        COMMANDS.putIfAbsent("trainNumberSubmit", this::findByTrainId);
+        COMMANDS.putIfAbsent("trainDestinationSubmit", this::findTrainByRoute);
+        COMMANDS.putIfAbsent("allTrainSubmit", this::findAllTrains);
     }
 
     @Override
@@ -36,5 +38,93 @@ public class SearchCommand implements Command {
         SearchTrainService.setStationList(request);
 
         return "/searchTrains.jsp";
+    }
+
+    private void findByTrainId(HttpServletRequest request) {
+        LOG.debug("Search trains by id");
+
+        int trainNumber = Integer.parseInt(request.getParameter("trainNumber"));
+
+        List<Train> trains = SearchTrainService.findTrainById(trainNumber);
+
+        setTravelDate(trains);
+        request.setAttribute("trainList", trains);
+    }
+
+    private void findTrainByRoute(HttpServletRequest request) {
+        String stationFrom = request.getParameter("departureStation");
+        String stationTo = request.getParameter("destinationStation");
+
+        List<Train> trains = SearchTrainService.findAllTrains();
+
+        trains = trains.stream().filter(train ->
+                checkTrainRoute(train, stationFrom, stationTo))
+                .collect(Collectors.toList());
+
+        setTravelDate(trains);
+
+        request.setAttribute("trainList", trains);
+        LOG.debug("Search trains by route");
+    }
+
+    private void findAllTrains(HttpServletRequest request) {
+        List<Train> trains = SearchTrainService.findAllTrains();
+
+        setTravelDate(trains);
+        request.setAttribute("trainList", trains);
+
+        LOG.debug("Search all trains");
+    }
+
+    private static boolean checkTrainRoute(Train train, String from, String to) {
+        LOG.debug("Check trains by route");
+
+        List<Route> trainRoute = train.getRouteList();
+
+        for (int i = 0; i < trainRoute.size() - 1; i++) {
+            if (trainRoute.get(i).getStation().getNameEN().equals(from)
+                    || trainRoute.get(i).getStation().getNameUA().equals(from)) {
+                for (int j = i + 1; j < trainRoute.size(); j++) {
+                    if (trainRoute.get(j).getStation().getNameEN().equals(to)
+                            || trainRoute.get(j).getStation().getNameUA().equals(to)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private void setTravelDate(List<Train> trains) {
+        Calendar calendar = Calendar.getInstance();
+
+        for (Train train : trains) {
+            Timestamp firstStationDepartureTime = train.getDepartureRoute().getDepartureTime();
+            Calendar departureCalendar = Calendar.getInstance();
+
+            departureCalendar.setTimeInMillis(firstStationDepartureTime.getTime());
+
+            calendar.set(Calendar.HOUR_OF_DAY, departureCalendar.get(Calendar.HOUR_OF_DAY));
+            calendar.set(Calendar.MINUTE, departureCalendar.get(Calendar.MINUTE));
+            calendar.set(Calendar.SECOND, departureCalendar.get(Calendar.SECOND));
+            calendar.set(Calendar.MILLISECOND, departureCalendar.get(Calendar.MILLISECOND));
+
+            train.getStations().forEach(route -> {
+                Calendar arrivalTime = Calendar.getInstance();
+                Calendar departureTime = Calendar.getInstance();
+
+                arrivalTime.setTimeInMillis(route.getArrivalTime().getTime());
+                departureTime.setTimeInMillis(route.getDepartureTime().getTime());
+
+                long arrivalTimeDiffer = arrivalTime.getTimeInMillis() - firstStationDepartureTime.getTime();
+                long departureTimeDiffer = departureTime.getTimeInMillis() - firstStationDepartureTime.getTime();
+
+                route.setArrivalTime(new Timestamp(calendar.getTimeInMillis() + arrivalTimeDiffer));
+                route.setDepartureTime(new Timestamp(calendar.getTimeInMillis() + departureTimeDiffer));
+            });
+        }
+
+        LOG.debug("Set travel date");
     }
 }
--- src/main/java/ua/training/model/service/SearchTrainService.java
+++ src/main/java/ua/training/model/service/SearchTrainService.java
@@ -6,119 +6,46 @@ import ua.training.model.dao.DaoFactory;
 import ua.training.model.dao.StationDao;
 import ua.training.model.dao.TrainDao;
 import ua.training.model.dao.implementation.JDBCDaoFactory;
-import ua.training.model.entity.Route;
 import ua.training.model.entity.Station;
 import ua.training.model.entity.Train;
 
 import javax.servlet.http.HttpServletRequest;
-import java.sql.Timestamp;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class SearchTrainService {
     private static final Logger LOG = LogManager.getLogger(SearchTrainService.class);
 
-    public static void findTrainById(HttpServletRequest request) {
+    public static List<Train> findTrainById(int trainNumber) {
         DaoFactory daoFactory = JDBCDaoFactory.getInstance();
+        List<Train> trainList = new ArrayList<>();
 
         try (TrainDao trainDao = daoFactory.createTrainDao()) {
-            int trainNumber = Integer.parseInt(request.getParameter("trainNumber"));
-            List<Train> trainList = new ArrayList<>();
             trainList.add(trainDao.findById(trainNumber));
-            setTravelDate(trainList);
-            request.setAttribute("trainList", trainList);
-            LOG.debug("Search all trains");
+
+            LOG.debug("Search train by number");
         } catch (Exception e) {
             LOG.debug("Exception while execute() Find by id");
-            LOG.error("Exception while execute() Find by id: {}",
-                    Arrays.toString(e.getStackTrace()));
+            LOG.error("Exception while execute() Find by id: {}", e);
         }
+
+        return trainList;
     }
 
-    private static void setTravelDate(List<Train> trains) {
-        Calendar calendar = Calendar.getInstance();
-
-        for (Train train : trains) {
-            Timestamp firstStationDepartureTime = train.getDepartureRoute().getDepartureTime();
-            Calendar departureCalendar = Calendar.getInstance();
-
-            departureCalendar.setTimeInMillis(firstStationDepartureTime.getTime());
-
-            calendar.set(Calendar.HOUR_OF_DAY, departureCalendar.get(Calendar.HOUR_OF_DAY));
-            calendar.set(Calendar.MINUTE, departureCalendar.get(Calendar.MINUTE));
-            calendar.set(Calendar.SECOND, departureCalendar.get(Calendar.SECOND));
-            calendar.set(Calendar.MILLISECOND, departureCalendar.get(Calendar.MILLISECOND));
-
-            train.getStations().forEach(route -> {
-                Calendar arrivalTime = Calendar.getInstance();
-                Calendar departureTime = Calendar.getInstance();
-
-                arrivalTime.setTimeInMillis(route.getArrivalTime().getTime());
-                departureTime.setTimeInMillis(route.getDepartureTime().getTime());
-
-                long arrivalTimeDiffer = arrivalTime.getTimeInMillis() - firstStationDepartureTime.getTime();
-                long departureTimeDiffer = departureTime.getTimeInMillis() - firstStationDepartureTime.getTime();
-
-                route.setArrivalTime(new Timestamp(calendar.getTimeInMillis() + arrivalTimeDiffer));
-                route.setDepartureTime(new Timestamp(calendar.getTimeInMillis() + departureTimeDiffer));
-            });
-        }
-    }
-
-    public static void findTrainByRoute(HttpServletRequest request) {
-        String stationFrom = request.getParameter("departureStation");
-        String stationTo = request.getParameter("destinationStation");
+    public static List<Train> findAllTrains() {
         DaoFactory daoFactory = JDBCDaoFactory.getInstance();
+        List<Train> trains = new ArrayList<>();
 
         try (TrainDao trainDao = daoFactory.createTrainDao()) {
-            List<Train> trains = trainDao.findAll();
+            trains = trainDao.findAll();
 
-            trains = trains.stream().filter(train ->
-                    checkTrainRoute(train, stationFrom, stationTo))
-                    .collect(Collectors.toList());
-
-            setTravelDate(trains);
-
-            request.setAttribute("trainList", trains);
-            LOG.debug("Search all trains");
-        } catch (Exception e) {
-            LOG.debug("Exception while execute() find Train by Route");
-            LOG.error("Exception while execute() find Train by Route: {}",
-                    Arrays.toString(e.getStackTrace()));
-        }
-    }
-
-    private static boolean checkTrainRoute(Train train, String from, String to) {
-        List<Route> trainRoute = train.getRouteList();
-
-        for (int i = 0; i < trainRoute.size() - 1; i++) {
-            if (trainRoute.get(i).getStation().getNameEN().equals(from)
-                    || trainRoute.get(i).getStation().getNameUA().equals(from)) {
-                for (int j = i + 1; j < trainRoute.size(); j++) {
-                    if (trainRoute.get(j).getStation().getNameEN().equals(to)
-                            || trainRoute.get(j).getStation().getNameUA().equals(to)) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-        }
-        return false;
-    }
-
-    public static void findAllTrains(HttpServletRequest request) {
-        DaoFactory daoFactory = JDBCDaoFactory.getInstance();
-
-        try (TrainDao trainDao = daoFactory.createTrainDao()) {
-            List<Train> trains = trainDao.findAll();
-            setTravelDate(trains);
-            request.setAttribute("trainList", trains);
             LOG.debug("Search all trains");
         } catch (Exception e) {
             LOG.debug("Exception while execute() allTrainSubmit getAllTrain");
             LOG.error("Exception while execute() allTrainSubmit getAllTrain: {}",
                     Arrays.toString(e.getStackTrace()));
         }
+
+        return trains;
     }
 
     public static void setStationList(HttpServletRequest request) {
