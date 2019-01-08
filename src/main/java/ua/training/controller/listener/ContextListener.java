efda44a93be3e46e44458a986d046ef4a90ae94a
Dean4iq <denis.ua22@gmail.com>
2019-01-08 21:34:48 +0200

    Refactored

--- src/main/java/ua/training/controller/listener/ContextListener.java
+++ src/main/java/ua/training/controller/listener/ContextListener.java
@@ -1,5 +1,8 @@
 package ua.training.controller.listener;
 
+import ua.training.model.util.AttributeResourceManager;
+import ua.training.model.util.AttributeSources;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.http.HttpSession;
@@ -7,18 +10,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ContextListener implements ServletContextListener {
+    private AttributeResourceManager attrManager = AttributeResourceManager.INSTANCE;
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         Set<String> activeUsers = new HashSet<>();
         Set<HttpSession> activeSessions = new HashSet<>();
-        servletContextEvent.getServletContext().setAttribute("loggedUsers", activeUsers);
-        servletContextEvent.getServletContext().setAttribute("activeSessions", activeSessions);
+
+        servletContextEvent.getServletContext()
+                .setAttribute(attrManager.getString(AttributeSources.LOGGED_USERS_CONTEXT), activeUsers);
+        servletContextEvent.getServletContext()
+                .setAttribute(attrManager.getString(AttributeSources.ACTIVE_SESSIONS), activeSessions);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        servletContextEvent.getServletContext().removeAttribute("loggedUsers");
-        servletContextEvent.getServletContext().removeAttribute("activeSessions");
+        servletContextEvent.getServletContext()
+                .removeAttribute(attrManager.getString(AttributeSources.LOGGED_USERS_CONTEXT));
+        servletContextEvent.getServletContext()
+                .removeAttribute(attrManager.getString(AttributeSources.ACTIVE_SESSIONS));
     }
 
 }
--- src/main/java/ua/training/controller/listener/SessionListener.java
+++ src/main/java/ua/training/controller/listener/SessionListener.java
@@ -3,7 +3,10 @@ package ua.training.controller.listener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.training.model.service.PurchaseService;
+import ua.training.model.util.AttributeResourceManager;
+import ua.training.model.util.AttributeSources;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
@@ -17,18 +20,21 @@ import java.util.Set;
 
 public class SessionListener implements HttpSessionListener {
     public static final Logger LOG = LogManager.getLogger(SessionListener.class);
+    private AttributeResourceManager attrManager = AttributeResourceManager.INSTANCE;
 
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         Map<String, String> menuItems = new LinkedHashMap<>();
+        ServletContext context = httpSessionEvent.getSession().getServletContext();
 
         menuItems.put("btn.login", "login");
         menuItems.put("btn.register", "register");
 
-        Set<HttpSession> sessionSet = (Set<HttpSession>) httpSessionEvent
-                .getSession().getServletContext().getAttribute("activeSessions");
+        Set<HttpSession> sessionSet = (Set<HttpSession>) context
+                .getAttribute(attrManager.getString(AttributeSources.ACTIVE_SESSIONS));
         sessionSet.add(httpSessionEvent.getSession());
-        httpSessionEvent.getSession().setAttribute("userbar", menuItems);
+        httpSessionEvent.getSession()
+                .setAttribute(attrManager.getString(AttributeSources.USERBAR), menuItems);
 
         LOG.info("New session created");
         LOG.debug("sessionCreated()");
@@ -36,23 +42,26 @@ public class SessionListener implements HttpSessionListener {
 
     @Override
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
-        HashSet<String> loggedUsers = (HashSet<String>) httpSessionEvent
-                .getSession().getServletContext().getAttribute("loggedUsers");
-        Set<HttpSession> sessionSet = (Set<HttpSession>) httpSessionEvent
-                .getSession().getServletContext().getAttribute("activeSessions");
+        HttpSession session = httpSessionEvent.getSession();
+        ServletContext context = session.getServletContext();
+
+        HashSet<String> loggedUsers = (HashSet<String>) context
+                .getAttribute(attrManager.getString(AttributeSources.LOGGED_USERS_CONTEXT));
+        Set<HttpSession> sessionSet = (Set<HttpSession>) context
+                .getAttribute(attrManager.getString(AttributeSources.ACTIVE_SESSIONS));
 
         checkTransactions(httpSessionEvent);
         sessionSet.remove(httpSessionEvent.getSession());
 
-        if (httpSessionEvent.getSession().getAttribute("User") != null) {
-            String userName = (String) httpSessionEvent.getSession()
-                    .getAttribute("User");
+        if (session.getAttribute(attrManager.getString(AttributeSources.ROLE_USER)) != null) {
+            String userName = (String) session
+                    .getAttribute(attrManager.getString(AttributeSources.ROLE_USER));
             loggedUsers.remove(userName);
 
             LOG.info("User's '{}' session destroyed", userName);
-        } else if (httpSessionEvent.getSession().getAttribute("Admin") != null) {
-            String userName = (String) httpSessionEvent.getSession()
-                    .getAttribute("Admin");
+        } else if (session.getAttribute(attrManager.getString(AttributeSources.ROLE_ADMIN)) != null) {
+            String userName = (String) session
+                    .getAttribute(attrManager.getString(AttributeSources.ROLE_ADMIN));
             loggedUsers.remove(userName);
 
             LOG.info("Admin's '{}' session destroyed", userName);
@@ -61,12 +70,13 @@ public class SessionListener implements HttpSessionListener {
         LOG.debug("sessionDestroyed()");
     }
 
-    private void checkTransactions(HttpSessionEvent httpSessionEvent){
-        if (httpSessionEvent.getSession().getAttribute("Ticket") != null
-                && httpSessionEvent.getSession().getAttribute("ticketConnection") != null) {
+    private void checkTransactions(HttpSessionEvent httpSessionEvent) {
+        HttpSession session = httpSessionEvent.getSession();
+        if (session.getAttribute(attrManager.getString(AttributeSources.TICKET_PURCHASE)) != null
+                && session.getAttribute(attrManager.getString(AttributeSources.TICKET_CONNECTION)) != null) {
 
-            try (Connection connection =
-                         (Connection) httpSessionEvent.getSession().getAttribute("ticketConnection")) {
+            try (Connection connection = (Connection) session
+                    .getAttribute(attrManager.getString(AttributeSources.TICKET_CONNECTION))) {
                 if (!connection.isClosed()) {
                     PurchaseService.declinePurchasing(connection);
                 }
