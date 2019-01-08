dddf21c64f02f6b1237c171dcfacc0631b62b643
Dean4iq <denis.ua22@gmail.com>
2019-01-08 20:59:37 +0200

    Refactored

--- src/main/java/ua/training/controller/filter/AdminFilter.java
+++ src/main/java/ua/training/controller/filter/AdminFilter.java
@@ -2,6 +2,8 @@ package ua.training.controller.filter;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ua.training.model.util.AttributeResourceManager;
+import ua.training.model.util.AttributeSources;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +13,7 @@ import java.io.IOException;
 
 public class AdminFilter implements Filter {
     private static final Logger LOG = LogManager.getLogger(AdminFilter.class);
+    private AttributeResourceManager attrManager = AttributeResourceManager.INSTANCE;
 
     @Override
     public void init(FilterConfig filterConfig) {
@@ -24,7 +27,7 @@ public class AdminFilter implements Filter {
         final HttpServletResponse httpResponse = (HttpServletResponse) response;
         HttpSession session = httpRequest.getSession(true);
 
-        if (session.getAttribute("Admin") == null) {
+        if (session.getAttribute(attrManager.getString(AttributeSources.ROLE_ADMIN)) == null) {
             LOG.debug("AdminFilter doFilter() loginRequest");
             httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
         } else {
