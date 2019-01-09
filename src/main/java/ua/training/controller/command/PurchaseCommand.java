3f469b29e9f1674146b4669978210cdaac82f105
Dean4iq <denis.ua22@gmail.com>
2019-01-09 14:23:28 +0200

    Creating documentation for class

--- src/main/java/ua/training/controller/command/PurchaseCommand.java
+++ src/main/java/ua/training/controller/command/PurchaseCommand.java
@@ -64,8 +64,10 @@ public class PurchaseCommand implements Command {
 
     /**
      * Checks if seat data for purchasing and ticket data are already exists
-     * @param request
-     * @return
+     *
+     * @param request provides user data and links to session
+     * @return link to purchase page after successful data processing or link
+     *         to error page if seat is occupied
      */
     private String setTicketData(HttpServletRequest request) {
         LOG.debug("Setting up ticket");
@@ -90,6 +92,7 @@ public class PurchaseCommand implements Command {
             PurchaseService.startPurchaseTransaction(ticket, request);
         } else if (!PurchaseService.isSeatFree(seatNumber, travelDate)
                 && session.getAttribute(attrManager.getString(AttributeSources.TICKET_PURCHASE)) == null) {
+            session.removeAttribute(attrManager.getString(AttributeSources.SEAT_PURCHASE));
             return "redirect: /exception";
         }
 
@@ -107,19 +110,34 @@ public class PurchaseCommand implements Command {
         return "/WEB-INF/user/purchase.jsp";
     }
 
+    /**
+     * Calls connection commit method to commit the ticket in DB
+     *
+     * @param request provides data for purchasing
+     */
     private void acceptPurchasing(HttpServletRequest request) {
         PurchaseService.confirmPurchasing(request);
-        processPageAttributes(request);
+        removePageAttributes(request);
         LOG.debug("Purchasing accepted");
     }
 
+    /**
+     * Calls connection rollback method to rollback the purchase transaction in DB
+     *
+     * @param request provides data for purchasing
+     */
     private void declinePurchasing(HttpServletRequest request) {
         PurchaseService.declinePurchasing((Connection) request.getSession().getAttribute("ticketConnection"));
-        processPageAttributes(request);
+        removePageAttributes(request);
         LOG.debug("Purchasing declined");
     }
 
-    private void processPageAttributes(HttpServletRequest request) {
+    /**
+     * Removes all purchasing info from session after transaction
+     *
+     * @param request links to session
+     */
+    private void removePageAttributes(HttpServletRequest request) {
         HttpSession session = request.getSession();
 
         session.removeAttribute(attrManager.getString(AttributeSources.TICKET_PURCHASE));
@@ -128,6 +146,14 @@ public class PurchaseCommand implements Command {
         session.removeAttribute(attrManager.getString(AttributeSources.TICKET_CONNECTION));
     }
 
+    /**
+     * Creates ticket with all data about travel requested by user
+     *
+     * @param request provides all data
+     * @param seatNumber seat number in trains wagon to occupy
+     * @param travelDate date when travel will begin
+     * @return ticket with all data about travel
+     */
     private Ticket initializeTicket(HttpServletRequest request, int seatNumber,
                                     Calendar travelDate) {
         HttpSession session = request.getSession();
@@ -164,6 +190,13 @@ public class PurchaseCommand implements Command {
         return ticket;
     }
 
+    /**
+     * Provides the train with all necessary information about travel as
+     * submitted travel date, user travel route
+     *
+     * @param train train that should be filled with travel data
+     * @param request provides data about travel
+     */
     private void setUpTrain(Train train, HttpServletRequest request) {
         HttpSession session = request.getSession();
 
@@ -179,6 +212,13 @@ public class PurchaseCommand implements Command {
         setTravelDateTime(train, tripDate, lang);
     }
 
+    /**
+     * Formats date in String to date in Calendar
+     * date in String should be formatted as yyyy-MM-dd
+     *
+     * @param date date that should be converted to Calendar
+     * @return Calendar with a year, month and day
+     */
     private Calendar getCalendarForDate(String date) {
         Calendar calendar = Calendar.getInstance();
         String[] splittedDate = date.split("-");
@@ -190,6 +230,13 @@ public class PurchaseCommand implements Command {
         return calendar;
     }
 
+    /**
+     * Formats ticket submitted travel date to locale language variable
+     *
+     * @param ticket where locale date should be stored
+     * @param request provides language info and travel date
+     * @see DateFormat
+     */
     private void setLocaleTime(Ticket ticket, HttpServletRequest request) {
         HttpSession session = request.getSession();
 
@@ -202,7 +249,7 @@ public class PurchaseCommand implements Command {
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL, locale);
         DateFormat timeFormat = DateFormat.getTimeInstance(DateFormat.SHORT, locale);
 
-        ticket.setFormattedTravelDate(dateFormat.format(setCalendar(tripDate).getTime()));
+        ticket.setFormattedTravelDate(dateFormat.format(getCalendarForDate(tripDate).getTime()));
 
         train.getRouteList().forEach(route -> {
             route.setLocaleArrivalTime(setFormattedDateTime(route.getArrivalTime(),
@@ -212,17 +259,14 @@ public class PurchaseCommand implements Command {
         });
     }
 
-    private Calendar setCalendar(String date) {
-        Calendar calendar = Calendar.getInstance();
-        String[] splittedDate = date.split("-");
-
-        calendar.set(Calendar.YEAR, Integer.parseInt(splittedDate[0]));
-        calendar.set(Calendar.MONTH, Integer.parseInt(splittedDate[1]) - 1);
-        calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(splittedDate[2]));
-
-        return calendar;
-    }
-
+    /**
+     * Formats date and time in Timestamp to String
+     *
+     * @param dateTime provided date and time
+     * @param dateFormat format rule to convert date
+     * @param timeFormat format rule to convert time
+     * @return string with formatted date and time, separated by *
+     */
     private String setFormattedDateTime(Timestamp dateTime, DateFormat dateFormat,
                                         DateFormat timeFormat) {
         java.util.Date date = new java.util.Date(dateTime.getTime());
@@ -235,6 +279,13 @@ public class PurchaseCommand implements Command {
         return stringBuilder.toString();
     }
 
+    /**
+     * Creating user route limited by selected stations
+     *
+     * @param train contains whole route information
+     * @param from the station form where user decided to depart
+     * @param to the station to where user decided to arrive
+     */
     private void setUserRoute(Train train, String from, String to) {
         List<Route> routeList = train.getRouteList();
 
@@ -254,6 +305,15 @@ public class PurchaseCommand implements Command {
         }
     }
 
+    /**
+     * Sets train departure and arrival time for the each station on the whole
+     * route
+     *
+     * @param train the train that should provide route list for setup
+     * @param date ticket travel date
+     * @param language selected language settings in session to convert date
+     *                 info in national variable
+     */
     private void setTravelDateTime(Train train, String date, String language) {
         Locale locale = new Locale(language);
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL, locale);
@@ -275,6 +335,14 @@ public class PurchaseCommand implements Command {
 
     }
 
+    /**
+     * Creates calendar with a year, moth and day from date string field and
+     * hours, minutes and seconds from train timestamp
+     *
+     * @param date provides ticket date in yyyy-MM-dd format
+     * @param trainTime provides train time on some event (departure/arrival)
+     * @return Calendar with year, month, day, hour, minute and seconds
+     */
     private Calendar setUpCalendar(String date, Timestamp trainTime) {
         Calendar calendar = Calendar.getInstance();
         String[] splittedDate = date.split("-");
@@ -288,12 +356,26 @@ public class PurchaseCommand implements Command {
         return calendar;
     }
 
+    /**
+     * Calls method to convert currency info into local currency variable
+     *
+     * @param ticket contains cost info
+     * @param request provides session language info
+     */
     private void setLocaleCurrency(Ticket ticket, HttpServletRequest request) {
         final String language = (String) request.getSession()
                 .getAttribute(attrManager.getString(AttributeSources.LANGUAGE));
         ticket.setLocaleCost(moneyFormatter(language, ticket.getCost()));
     }
 
+    /**
+     * Converts currency info into local currency variable (without a currency
+     * sign)
+     *
+     * @param language language to create locale variable for convert
+     * @param value int cost value that should be converted
+     * @return converted string with locale rules
+     */
     private String moneyFormatter(String language, int value) {
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance(new Locale(language));
         DecimalFormatSymbols decimalFormatSymbols = ((DecimalFormat) numberFormat).getDecimalFormatSymbols();
