156282e885602acac0b1eb8db146c5046d7a92ac
Dean4iq <denis.ua22@gmail.com>
2019-01-11 11:43:53 +0200

    Unused import removed

--- src/main/java/ua/training/model/dao/implementation/JDBCDaoFactory.java
+++ src/main/java/ua/training/model/dao/implementation/JDBCDaoFactory.java
@@ -3,7 +3,6 @@ package ua.training.model.dao.implementation;
 import ua.training.model.dao.*;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCDaoFactory extends DaoFactory {
